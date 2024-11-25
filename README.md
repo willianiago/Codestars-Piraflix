f24642d3d1004cf3101a1cbf988e83260dfeb30e
ALUNO SESI CABO <aluno.sesicabo@SISTEMAFIEPE.LOCAL>
2024-11-25 17:53:29 -0300

    estruturação

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Codestars-Piraflix
+# pacotes instalados
+
+pip install flask
+pip install flask SQLA1chemy werkzeug
+pip install flask-login
---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class config:
+    SQLALCHEMY_DATABASE_URI = 'sqlte://colegio.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = '12345678'
